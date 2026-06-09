787cb5a6772b65e314b49f2ce178128a937bf9ac
Lahindu Ishan <lahindu2001ishan@gmail.com>
2026-06-09 17:22:57 +0530

    update .env

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -6,6 +6,7 @@ const fundsRouter = require("./Routes/FundsRoutes"); // New import
 const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
+require('dotenv').config();
 const app = express();
 
 // ------------------- MIDDLEWARE -------------------
@@ -28,9 +29,10 @@ app.use("/funds", fundsRouter); // New route
 
 // ------------------- DATABASE -------------------
 mongoose
-  .connect("mongodb+srv://adminSelfme:P40YIFy04Am8rnDe@cluster0.4bp3tta.mongodb.net/shop")
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .then(() => {
-    app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => console.log("🚀 Server running on port " + port));
   })
   .catch((err) => console.log(err));
